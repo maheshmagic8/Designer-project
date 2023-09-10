481e88c1831bde79781a829495e21bd1182a6460
maheshmagic8 <88144775+maheshmagic8@users.noreply.github.com>
2023-09-10 19:30:48 +0530

    Final_3
    creating the hidden menu and other login pages DT-10-09-2023

--- js/Designer.js
+++ js/Designer.js
@@ -1,9 +1,33 @@
 const hamgurgur  = document.querySelector(".hamgurgur");
-const hidden_manu_list = document.querySelector(".hidden_manu_list");
+const hidden_manu = document.querySelector(".hide-manu");
+const login = document.querySelector(".login");
+const for_login = document.querySelector(".for_login");
+const for_login_input = document.querySelector(".for_login_input");
 
 
-hamgurgur.addEventListener("click",()=>{
+hamgurgur.addEventListener("click", ()=>{
+    hidden_manu.classList.toggle("revil");
     hamgurgur.classList.toggle("active");
-    hidden_manu_list.classList.toggle("active_hidden_manu_list")
 })
+
+login.addEventListener("click", ()=>{
+    for_login.classList.toggle("log_unlock");
+})
+
+
+let cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let small = "abcdefghijklmnopqrstuvwxyz";
+let spacial = "`!@#$%^&*()_+{}:|>?<";
+
+
+
+function Submit(){
+    if(for_login_input.innerHTML === cap,small,spacial){
+        alert("Please Enter The Username")
+    }else{
+        
+    }
+
+    
+}
 
