d91f3dc8bb13e896f2b118b66436087e3a906b61
maheshmagic8 <88144775+maheshmagic8@users.noreply.github.com>
2023-09-11 23:10:46 +0530

    final_4
    11-09-2023

--- js/Designer.js
+++ js/Designer.js
@@ -2,7 +2,10 @@ const hamgurgur  = document.querySelector(".hamgurgur");
 const hidden_manu = document.querySelector(".hide-manu");
 const login = document.querySelector(".login");
 const for_login = document.querySelector(".for_login");
-const for_login_input = document.querySelector(".for_login_input");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const submit_btn = document.querySelector(".submit-btn");
+const form_m = document.querySelector("#form_m");
 
 
 hamgurgur.addEventListener("click", ()=>{
@@ -15,19 +18,16 @@ login.addEventListener("click", ()=>{
 })
 
 
-let cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let small = "abcdefghijklmnopqrstuvwxyz";
-let spacial = "`!@#$%^&*()_+{}:|>?<";
 
+submit_btn.addEventListener("click" , (e)=>{
+    e.preventDefault();
 
+    swal({
+        title: "Logind!",
+        text: "You will be updated soon!",
+        icon: "success",
+        button: "OK!",
+    });
+})
 
-function Submit(){
-    if(for_login_input.innerHTML === cap,small,spacial){
-        alert("Please Enter The Username")
-    }else{
-        
-    }
-
-    
-}
 
