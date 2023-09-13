eb7a2c333f93e2b806cffa7c3530b3199ea13034
maheshmagic8 <88144775+maheshmagic8@users.noreply.github.com>
2023-09-13 16:24:34 +0530

    final_5
    adding and modify menu 13-09-2023

--- js/Designer.js
+++ js/Designer.js
@@ -6,7 +6,10 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const submit_btn = document.querySelector(".submit-btn");
 const form_m = document.querySelector("#form_m");
-
+const for_login_Xicon = document.querySelector("#for_login_Xicon")
+const for_signup_Xicon = document.querySelector("#for_signup_Xicon")
+const for_signup = document.querySelector(".for_signup");
+const signup = document.querySelector(".signup");
 
 hamgurgur.addEventListener("click", ()=>{
     hidden_manu.classList.toggle("revil");
@@ -14,13 +17,25 @@ hamgurgur.addEventListener("click", ()=>{
 })
 
 login.addEventListener("click", ()=>{
-    for_login.classList.toggle("log_unlock");
+    for_login.style.transform = "scale(1)";
 })
 
+for_login_Xicon.addEventListener("click", ()=>{
+    for_login.style.transform = "scale(0)";
+})
+
+for_signup_Xicon.addEventListener("click", ()=>{
+    for_signup.style.transform = "scale(0)";
+})
+
+signup.addEventListener("click", ()=>{
+    for_signup.style.transform = "scale(1)";
+})
 
 
 submit_btn.addEventListener("click" , (e)=>{
     e.preventDefault();
+ 
 
     swal({
         title: "Logind!",
